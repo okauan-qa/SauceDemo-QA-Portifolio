74acaabaee43b64da798d5ce73251e7b4182b46f
Kauan de Oliveira <okauan652@gmail.com>
2026-08-27 04:30:55 -0300

    test(inventory): add Z to A sorting scenario for problem user #BUG-002

--- cypress/e2e/problem-user.cy.js
+++ cypress/e2e/problem-user.cy.js
@@ -87,12 +87,12 @@ cy.visit('https://www.saucedemo.com/')
 
 
       cy.get('[data-test^="add-to-cart"]')
-      .should('have.length', 0) //Resultado deveria ser 0, porém o cypress retornou que tem 3 botões com o status "Add to cart"
+      .should('have.length', 0) //Resultado deveria ser 0, porém o cypress retornou que tem 3 botões com o status "Add to cart" BUG-001
     })
       it.only('deve ordenar a lista de A a Z', () => {
       cy.get('[data-test="product-sort-container"]')
       .select("za")
-      .should('have.value', 'za')
+      .should('have.value', 'za') //Resultado deveria ordenar a lista para Z - A, porém permance A - Z BUG-002
     })
 
   })
