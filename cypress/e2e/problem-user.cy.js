e8a0f1980f284c5df0823467dfed7a228ec236bf
Kauan de Oliveira <okauan652@gmail.com>
2026-08-28 00:59:05 -0300

    test(checkout): validate personal information for problem user BUG-005

--- cypress/e2e/problem-user.cy.js
+++ cypress/e2e/problem-user.cy.js
@@ -29,7 +29,7 @@ cy.visit('https://www.saucedemo.com/')
     cy.get('.error-message-container').should('be.visible')
   })
 
-  describe('Testando página home', () =>{
+describe('Testando página home', () =>{
     beforeEach(() =>{
         cy.visit('https://www.saucedemo.com/')
         cy.problemUser()
@@ -38,12 +38,13 @@ cy.visit('https://www.saucedemo.com/')
         cy.contains('button','Add to cart').click()
         cy.get('.shopping_cart_link').click() //Entra no carrinho para verificar se o item está lá
     })
-    it('deve adicionar mais de um item no carrinho', () => {
+
+    it('deve adicionar dois itens especificos no carrinho', () => {
         cy.get('#add-to-cart-sauce-labs-backpack').click()
         cy.get('#add-to-cart-sauce-labs-bike-light').click()
         cy.get('.shopping_cart_link').click()
     })
-        it('deve adicionar mais de um item no carrinho, entrar no carrinho e remover item', () => {
+    it('deve adicionar mais de um item no carrinho, entrar no carrinho e remover item', () => {
         cy.get('#add-to-cart-sauce-labs-backpack').click()
         cy.get('#add-to-cart-sauce-labs-bike-light').click()
         cy.get('.shopping_cart_link').click() //Entra no carrinho para verificar se o item está lá
@@ -85,24 +86,32 @@ cy.visit('https://www.saucedemo.com/')
       .should('have.length.gt', 2) // deve ter mais de dois itens
       .click({ multiple: true }) // clica em multiplos
 
-
       cy.get('[data-test^="add-to-cart"]')
       .should('have.length', 0) //Resultado deveria ser 0, porém o cypress retornou que tem 3 botões com o status "Add to cart" BUG-001
     })
+
       it('deve ordenar a lista de A a Z', () => {
       cy.get('[data-test="product-sort-container"]')
       .select("za")
       .should('have.value', 'za') //Resultado deveria ordenar a lista para Z - A, porém permance A - Z BUG-002
     })
-      it.only('Deve ordenar produtos do menor para o maior preço', () => {
+    it('deve ordenar produtos do menor para o maior preço', () => {
         cy.get('[data-test="product-sort-container"]')
           .select('lohi')
           .should('have.value', 'lohi') //Resultado deveria ordenar a lista do menor para o maior preço, porém permance A - Z BUG-003
-      })
-      it('Deve ordenar produtos do maior para o menor preço', () => {
+    })
+    it('Deve ordenar produtos do maior para o menor preço', () => {
       cy.get('[data-test="product-sort-container"]')
       .select('hilo')
       .should('have.value', 'hilo') //Resultado deveria ordenar a lista do maior para o menor preço, porém permance A - Z BUG-004
+    })
+    it.only('deve adicionar um item no carrinho e finalizar a compra', () => {
+        cy.contains('button','Add to cart').click()
+        cy.get('.shopping_cart_link').click() //Entra no carrinho para verificar se o item está lá
+        cy.contains('button', 'Checkout').click()
+
+        cy.get('#first-name').type('Kauan').should('have.value', 'Kauan')
+        cy.get('#last-name').type('Brito').should('have.value', 'Brito') //Teste deveria escrever "Brito" no campo "last-name", e verificar o texto que foi escrito e retornar verdadeiro, porém retorna como falso. BUG-005
     })
 
   })
